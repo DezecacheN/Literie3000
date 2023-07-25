cd039025b8b5ffbbdb214891f19bcae7868e3101
Nicolas DEZECACHE <n.dezecache@gmail.com>
2023-07-25 09:24:36 +0200

    fichiers de base du site

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include("templates/header.php")
+?>
+
+<section class="page">
+
+    <div class="homeChoice">
+        <h2>
+            <a href="catalogue.php"> Voir notre catalogue </a>
+        </h2>
+    </div>
+
+    <div class="homeChoice">
+        <h2>
+            <a href="catalogue.php"> Modifier le catalogue </a>
+        </h2>
+    </div>
+
+</section>
+
+<?php
+include("templates/footer.php")
+?>
---
+++ templates/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Telefoot - La chaine du foot</title>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+
+    <link rel="stylesheet" href="css/icomoon.css" />
+    <link rel="stylesheet" href="css/main.css" />
+</head>
+
+<body>
+    <header>
+        <img src="logo/logo.png" />
+        <nav>
+            <ul>
+                <a href="./index.php" class="nav-link">Accueil</a>
+                <a href="./catalogue.php" class="nav-link">Voir Catalogue</a>
+                <a href="./edit_catalogue.php" class="nav-link">Gérer Catalogue</a>
+            </ul>
+        </nav>
+
+    </header>
